be5228a79311fb32d211089d527287e5f013deb2
justinn leong <justinnjamesleong@gmail.com>
2022-05-30 13:23:41 +0800

    cloudainry

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -26,6 +26,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id, :photos)
   end
 end
